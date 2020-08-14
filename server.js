54bae1aa0db9d90d0eed7fca1f0c60a322da8e3d
NetanMangal <imnetanmangal@gmail.com>
2020-08-15 03:29:09 +0530

    project setup

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
+const bcrypt = require("bcryptjs");
+const Joi = require("@hapi/joi");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const dbConnection = require("./src/dbConnection.js");
+const userRoutes = require("./src/routes/userRoutes.js");
+const msg = require("./config/messages.json");
+const app = express();
+const adminRoutes = require("./src/routes/adminRoutes.js")
+
+
+app.use(express.json());        //for parsing application/json
+app.use(express.urlencoded({    //for parsing application/x-www-form-urlencoded
+    "extended": true
+}));
+app.use(morgan("tiny"));
+
+app.use((req, res, next) => {
+    res.set("Access-Control-Allow-Origin", "*");
+    res.set("Access-Control-Allow-Headers", "*");
+    res.set("X-Developed-By", "ORGAN-DONATION-THEME-BACKEND-TEAM");
+
+    next();
+});
+
+// app.use(cors());
+// app.options('*', cors());
+
+
+app.use("/api/user", userRoutes);
+app.use("/api/admin", adminRoutes);
+
+app.get("/", (req, res) => {
+    res.send("We are on homepage ");
+});
+
+module.exports = app;
